90f855b48e0ad43cbcdfcd93b31706012af1e406
Zyle <poonyapat.y@ku.th>
2018-05-30 15:36:06 +0700

    remove unused code

--- Assets/Script/Flag.cs
+++ Assets/Script/Flag.cs
@@ -8,7 +8,6 @@ public class Flag : MonoBehaviour {
 			GameController.activatingPlayer = coll.GetComponent<PlayerData>();
 			GameController.activatingPlayer.IncreaseGameLevel();
 			GameController.LoadLevel (GameController.activatingPlayer.LatestGameLevel);
-			coll.gameObject.GetComponent<PlayerController> ().GotoSpawnPosition ();
 			if (coll.gameObject.GetComponent<PlayerController> ().weapon != null)
 				DontDestroyOnLoad (coll.gameObject.GetComponent<PlayerController> ().weapon.gameObject);
 		}
